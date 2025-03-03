4ec4ae5ede525e967a51dff8c88c5fb398fb715b
zakaria <zak0zak0zak@gmail.com>
2025-03-03 09:03:24 +0200

    enhance error handling

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,43 @@
-import { Body, Controller, Post, Res, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, Res, UseFilters, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
+import { SkipAuth } from './decorators/SkipAuth.decorator';
 
+@SkipAuth()
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @Post('register')
     async register(@Body() payload: CreateUserDto, @Res() res: Response) {
-        try {
-            const result = this.authService.register(payload);
-            return res.send({ result })
-        } catch (e) {
-            return res.send({ e })
-        }
+        const data = await this.authService.register(payload);
+
+        res.status(data.statusCode);
+
+        if (data.error)
+            return res.send(data);
+
+        return res.send(data)
     }
 
     @Post('login')
     async login(@Body() payload: LoginUserDto, @Res() res: Response) {
         const data = await this.authService.login(payload);
-        return res.send({ data })
+
+        res.status(data.statusCode);
+
+        if (data.error)
+            return res.send(data);
+
+        return res.send({ access_token: data })
     }
 
     @Post('logout')
-    async logout(@Res() res: Response) {
-        const result = this.authService.logout();
-        return res.send({ message: result })
+    logout(@Body() { token }: { token: string }, @Res() res: Response) {
+        this.authService.logout(token);
+        res.status(200);
+        res.send({ message: 'Logged out successfully' });
     }
 }
