51d8f59042900f62d56e47fa047a2ca07fb52301
zakaria <zak0zak0zak@gmail.com>
2025-03-03 02:17:37 +0200

    test connection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { RegisterUserDto } from './dto/register-user.dto';
+import { InjectModel } from "@nestjs/mongoose";
+import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { User } from 'src/schemas/User.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-    register(payload: RegisterUserDto) {
-        return `Register endpoint`
+    constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+    async register(payload: CreateUserDto) {
+        const newUser = new this.userModel(payload)
+        return await newUser.save()
     }
 
     login(payload: LoginUserDto) {
---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,31 @@
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(20)
+    username: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsStrongPassword({
+        minLength: 6,
+        minLowercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+        minUppercase: 1
+    })
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(12)
+    confirmPassword: string;
+}
